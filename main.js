bf9c61d65b6d995487b1f280a23f7ac764d1c6ec
hyeon-b <20201009@sungshin.ac.kr>
2022-05-17 14:18:22 +0000

    upload directory architecture

---
+++ main.js
@@ -0,0 +1,28 @@
+const express = require("express"),
+  app = express(),
+  //homeController = require("./controllers/homeController"),
+  errorController = require("./controllers/errorController"),
+  layouts = require("express-ejs-layouts");
+  
+app.set("view engine", "ejs");
+app.set("port", process.env.PORT || 3000);
+app.use(
+    express.urlencoded({
+      extended: false
+    })
+  );
+  app.use(express.json());
+  app.use(layouts);
+  app.use(express.static("public"));
+  
+  app.get("/", (req, res) => {      //기본 메인페이지 이동
+    res.render("index");
+  });
+  // 생성해야 할 controllers : 회원가입 / 등록 / 회원정보설정 / 
+
+  app.use(errorController.pageNotFoundError);
+  app.use(errorController.internalServerError);
+
+  app.listen(app.get("port"), () => {
+    console.log(`Server running at http://localhost:${app.get("port")}`);
+  });
