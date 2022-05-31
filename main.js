808909f7c3bb8b2f66f64fb0eb6c0b3c1503ae19
Minjin Kim <kkmjkim1528@gmail.com>
2022-05-31 12:24:08 +0000

    remove unnecessary codes

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
-const { Router } = require("express");
-const { signUpTerms } = require("./controllers/signUpController");
-
-
 const express = require("express"),
   app = express(),
   router = express.Router(),
   errorController = require("./controllers/errorController"),
   homeController=require("./controllers/homeController"),
   signUpController=require("./controllers/signUpController"),
-  enrollController = require("./controllers/enrollController"), //등록 컨트롤러
+  enrollController = require("./controllers/enrollController"),
   layouts = require("express-ejs-layouts"),
   methodOverride = require("method-override");
   
@@ -16,28 +12,19 @@ app.set("port", process.env.PORT || 80);
 app.set("view engine", "ejs");
 app.use("/public", express.static(__dirname + "/public"));
 
-
-
 router.use(
   methodOverride("_method", {
     methods: ["POST", "GET"]
   })
 );
-
-router.use(layouts);
-router.use(express.static("public"));
-
 router.use (
   express.urlencoded({
     extended : false
   })
 );
+router.use(layouts);
+router.use(express.static("public"));
 router.use(express.json());
-
-
-
-//router.get("/", homeController.index); //terms
-
 
 router.get("/", homeController.homePage);
 router.get("/enroll", enrollController.showEnroll);
@@ -53,12 +40,10 @@ router.post("/signUp_emailAuth", signUpController.emailAuth);
 router.post("/signUp_emailCert", signUpController.emailCert);
 router.get("/signUp_complete", signUpController.signUp_complete);
 
-
- 
 router.use(errorController.pageNotFoundError);
 router.use(errorController.internalServerError);
 
 app.use("/", router);
 app.listen(app.get("port"), () => {
-  console.log(`Server running at http://localhost:${app.get("port")}`);
+  console.log(`Server running at ${app.get("port")}`);
 });
