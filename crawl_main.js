051b71d433842c41519255168f00ee3d0f53acec
Minjin Kim <kkmjkim1528@gmail.com>
2022-06-12 06:44:09 +0000

    feat: send email after crawling

--- crawl_main.js
+++ crawl_main.js
@@ -7,9 +7,9 @@ const puppeteer = require("puppeteer"),
 db.sequelize.sync({alter: false});
 const Page = db.pageTBL;
 const Register = db.registerTBL;
+const User = db.userTBL;
 
 const save_dir = "./crawling_results/";
-let final_array = []
 
 // crawl & save it as an html file
 async function save_html(url, file_name) {
@@ -41,7 +41,6 @@ async function crawl_and_check(url) {
     console.log(`saved ${file_name}`);
 
     let result_array = await check_register(url, file_name);
-    // return `Done: ${url}`, final_array
     return result_array
 }
 
@@ -56,8 +55,6 @@ async function check_register(url_link, url_file) {  // TODO: simplify
     let satisfied = registrations.map(
         a => ({registerId: a.registerId, satisfied: false})
     );
-    // console.log("initialized satisfied array as:")
-    // console.log(satisfied)  // array of objects
 
     const $ = cheerio.load(fs.readFileSync(url_file));
     const whole_text = $.text();
@@ -82,23 +79,82 @@ function check_keyword_match(whole_text, keywords, logic) {
     }
 }
 
-// find URLs
-let url_array
-let failed_url_array = [];
-async function run_all_urls() {
-    let pages = await Page.findAll({raw: true})
-    // console.log(pages.every(page => page instanceof Page))  // false
-    // console.log("All pages:", JSON.stringify(pages, null, 2));
-    url_array = pages.map(a => a.url);  // ["url1", "url2", ..]
-    console.log(url_array)
-    run_loop()
+async function get_user_email(registerId) {
+    let register = await Register.findOne( {where: {registerId: registerId}} )
+    let user = await User.findOne( {where: {id: register["userId"]}})
+    return user["email"]
 }
 
+async function make_email_body(registerId) {
+    let register = await Register.findOne( {where: {registerId: registerId}})
+    return `
+    ${register["siteName"]} 사이트를 확인하세요!
+    내가 등록한 사이트 바로가기: ${register["pageUrl"]}
+
+    요청하신 키워드는: ${keywords}
+    키워드 포함 조건: ${register["notifyLogic"]}
+
+    알림 사유: 설정한 마감일 전 등록 조건 일치!
+
+    알림이 성공적으로 이루어졌으므로 등록은 삭제됩니다.
+
+    메추리알을 사용해주셔서 감사합니다.
+    `
+}
+
+async function make_email_title(registerId) {
+    let register = await Register.findOne( {where: {registerId: registerId}})
+    return `[메추리알 알림] ${register["siteName"]} 사이트를 확인하세요!`
+}
+
+async function send_register_email(receiver_email, email_title, email_body) {
+    require('dotenv').config();
+    const sendgrid = require('@sendgrid/mail');
+    sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+    try{
+        await sendgrid.send({
+            to: receiver_email,
+            from: 'ssc22.team.07@gmail.com',
+            subject: email_title,
+            text: email_body,
+        });
+        console.log("email sent successfully!")
+    } catch(err) {
+        console.log(err);
+    };
+}
+
+async function email_satisfied_register (all_array) {
+    for (let idx = 0; idx < all_array.length; idx++) {
+        if (all_array[idx]["satisfied"]) {
+            let registerId = all_array[idx]["registerId"]
+            let receiver_email = await get_user_email(registerId)
+            let email_body = await make_email_body(registerId)
+            let email_title = await make_email_title(registerId)
+            await send_register_email(receiver_email, email_title, email_body)
+            await delete_satisfied_register(registerId)
+        }
+    }
+}
+
+async function delete_satisfied_register(registerId) {
+    await Register.destroy({where: {registerId: registerId}})
+    console.log(`deleted registerId: ${registerId}`)
+}
+
+const TOTAL_REG_NUMBER = 3  // temporary
 function run_loop() {
+    let final_array = []
     for (let idx = 0; idx < url_array.length; idx++) {
         crawl_and_check(url_array[idx]).then((partial_array) => {
             final_array = final_array.concat(partial_array)
-            if (final_array.length >= 5) {console.log(final_array)}  // 등록이 총 6개라서 최종 배열만 출력하도록 임시로 처리 [{"regNumber": 1, satisfied:true},.. ]
+            console.log(final_array)
+            // 등록 개수에 따라 최종 배열만 출력하도록 임시로 처리
+            if (final_array.length >= TOTAL_REG_NUMBER) {
+                console.log("final array is")
+                console.log(final_array)
+                email_satisfied_register(final_array)  // TODO: await?
+            }
         }).catch((err) => {
             failed_url_array.push(url_array[idx]);
             console.log(err);
@@ -106,8 +162,16 @@ function run_loop() {
     }
 }
 
-// run_all_urls();
-
+// find URLs
+let url_array
+let failed_url_array = [];
+async function run_all_urls() {
+    let pages = await Page.findAll({raw: true})
+    url_array = pages.map(a => a.url);  // ["url1", "url2", ..]
+    console.log(url_array)
+    await run_loop()
+    console.log("========= Done! =========")
+}
 
 // *:*:0, *:*30 (30초마다 실행됨)
 const job = nodeCron.schedule("*/30 * * * * *", function () {
@@ -115,11 +179,10 @@ const job = nodeCron.schedule("*/30 * * * * *", function () {
     let hours = date_ob.getHours();
     let minutes = date_ob.getMinutes();
     let seconds = date_ob.getSeconds();
-    console.log(`${hours}:${minutes}:${seconds}`);
+    console.log(`running at: ${hours}:${minutes}:${seconds}`);
     failed_url_array = [];
     final_array = [];
     run_all_urls()
-    console.log("nodeCron is super cool!");
 }, {
     scheduled: false,
 });
