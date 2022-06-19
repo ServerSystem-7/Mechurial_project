7a7672f5f05acf5cd12905c21a414c76147ce39a
Minjin Kim <kkmjkim1528@gmail.com>
2022-06-19 09:35:47 +0000

    feat: email overdue registrations

--- crawl_main.js
+++ crawl_main.js
@@ -3,6 +3,7 @@ const puppeteer = require("puppeteer"),
     cheerio = require("cheerio"),
     db = require("./models/index"),
     nodeCron = require("node-cron"),
+    { Op } = require("sequelize"),
     utils = require("./utils");
 
 db.sequelize.sync({alter: false});
@@ -17,11 +18,11 @@ if (!fs.existsSync(save_dir)){
 
 // crawl & save as an html file
 async function save_html(url, file_name) {
-    console.log("============= save_html() =============")
     const browser = await puppeteer.launch({
         args: ['--no-sandbox', '--disable-setuid-sandbox'],  // no-sandbox b/c sudo
     });
     const page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(0);
     await page.goto(url, {waitUntil: 'networkidle2'});  // go to the page to parse
     await page.waitFor(3000);
     const html = await page.content();  // extract data from the page
@@ -33,7 +34,6 @@ async function save_html(url, file_name) {
 }
 
 async function check_register(url_link, url_file) {
-    console.log("============= check_register() =============")
     // all register instances of the url_link
     let registrations = await Register.findAll({
         where: {pageUrl: url_link},
@@ -48,22 +48,17 @@ async function check_register(url_link, url_file) {
         let cur_reg = registrations[id]
 
         let keywords = [cur_reg["key1"], cur_reg["key2"], cur_reg["key3"]]
-        keywords = keywords.filter((key) => {
+        let validKeywords = keywords.filter((key) => {
             key.length > 0
         })
-        console.log(49, keywords)
 
         let logic = registrations[id]["notifyLogic"]
-        satisfied[id]["satisfied"] = await check_keyword_match(url_file, keywords, logic)
+        satisfied[id]["satisfied"] = await check_keyword_match(url_file, validKeywords, logic)
     }
-    console.log("============= check_register() done =============")
-    console.log(53, satisfied)
     return satisfied
 }
 
 function check_keyword_match(url_file, keywords, logic) {
-    console.log("============= check_keyword_match() =============")
-    console.log(59, url_file, keywords, logic)
     const $ = cheerio.load(fs.readFileSync(url_file));
     const whole_text = $.text();
 
@@ -77,13 +72,11 @@ function check_keyword_match(url_file, keywords, logic) {
 }
 
 async function email_satisfied_register (all_array) {
-    console.log("====== email_satisfied_register() =========")
     for (let idx = 0; idx < all_array.length; idx++) {
         if (all_array[idx]["satisfied"] === true) {
             let registerId = all_array[idx]["registerId"]
-            console.log(92, "satisfied true regID: ", registerId)
             let receiver_email = await utils.get_user_email(registerId)
-            let email_body = await utils.make_email_body(registerId)  // 1
+            let email_body = await utils.make_email_body(registerId)
             let email_title = await utils.make_email_title(registerId)
             await utils.sendEmail(receiver_email, email_body, email_title)
             await delete_satisfied_register(registerId)
@@ -95,7 +88,6 @@ async function delete_satisfied_register(registerId) {
     try{
         let register = await Register.findOne({where: {registerId: registerId}});
         const url = register["pageUrl"];
-        console.log(url);
         await Register.destroy({where: {registerId: registerId}});
         console.log(`deleted registerId: ${registerId}`);
         let result= await Register.findOne({
@@ -113,12 +105,10 @@ async function delete_satisfied_register(registerId) {
     };
 }
 
-
 async function crawl_and_check(url) {
-    // file_name = save_dir + "file3.html"  // TODO: increment & path join?
     let current_time = new Date()
     let file_name = String(current_time.getTime()) + ".html"
-    let save_path = save_dir + utils.increment_path(file_name)  // temp
+    let save_path = save_dir + utils.increment_path(file_name)
     await save_html(url, save_path);
 
     let result_array = await check_register(url, save_path);
@@ -127,21 +117,48 @@ async function crawl_and_check(url) {
 
 async function run_all_urls() {
     let pages = await Page.findAll({raw: true})
-    let url_array = pages.map(a => a.url);  // ["url1", "url2", ..]
-    console.log("All URLs: ", url_array)
-
+    let url_array = pages.map(a => a.url);
     for (let idx = 0; idx < url_array.length; idx++) {
         crawl_and_check(url_array[idx])
     }
 }
 
+async function deleteOverdueRegistrations(current_date) {
+    let overdue_registrations = await Register.findAll({
+        where: {
+            dueDate: {
+                [Op.lt]: current_date
+            }
+        },
+        raw: true
+    })
+    for (let idx = 0; idx < overdue_registrations.length; idx++) {
+        let registerId = overdue_registrations[idx]["registerId"]
+        let receiver_email = await utils.get_user_email(registerId)
+        let email_title = await utils.make_email_title_overdue(registerId)
+        let email_body = await utils.make_email_body_overdue(registerId)
+        await utils.sendEmail(receiver_email, email_body, email_title)
+        await delete_satisfied_register(registerId)
+    }
+}
+
 // run every minute
-const job = nodeCron.schedule("0 * * * * *", function () {
+const job = nodeCron.schedule("0 * * * * *", async function () {
     let date_ob = new Date();
+    let SEOULTIMEZONE = 9 * 60 * 60
+    await date_ob.setTime(date_ob.getTime() + SEOULTIMEZONE);  // time zone is set
+
     let hours = date_ob.getHours();
     let minutes = date_ob.getMinutes();
     let seconds = date_ob.getSeconds();
     console.log(`running at: ${hours}:${minutes}:${seconds}`);
+
+    let dd = String(date_ob.getDate()).padStart(2, '0');
+    let mm = String(date_ob.getMonth() + 1).padStart(2, '0');  // January is 0
+    let yyyy = date_ob.getFullYear();
+    let current_date = `${yyyy}-${mm}-${dd}`
+
+    await deleteOverdueRegistrations(current_date)
     run_all_urls()
 }, {
     scheduled: false,
--- utils.js
+++ utils.js
@@ -2,7 +2,6 @@ const fs = require("fs"),
     db = require("./models/index");
 
 db.sequelize.sync({alter: false});
-const Page = db.pageTBL;
 const Register = db.registerTBL;
 const User = db.userTBL;
 
@@ -10,63 +9,87 @@ require('dotenv').config();
 const sendgrid = require('@sendgrid/mail');
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
+module.exports = {
+    sendEmail: async (receiver_email, body, title) => {
+        try{
+            await sendgrid.send({
+            to: receiver_email,
+            from: process.env.SENDGRID_SENDER_EMAIL,
+            subject: title,
+            text: body,
+        });
+        } catch(err) {
+            console.log(err);
+        };
+    },
 
-exports.sendEmail = async (receiver_email, body, title) => {
-    try{
-        await sendgrid.send({
-        to: receiver_email,
-        from: process.env.SENDGRID_SENDER_EMAIL,
-        subject: title,
-        text: body,
-    });
-    } catch(err) {
-        console.log(err);
-    };
-}
+    generateRandom: (min, max) => {
+        const randomNumber = Math.floor(Math.random() * (max-min+1)) + min;
+        return randomNumber;
+    },
 
-exports.generateRandom = (min, max) => {
-    const randomNumber = Math.floor(Math.random() * (max-min+1)) + min;
-    return randomNumber;
-}
+    increment_path: (path, overwrite = false) => {
+        if (overwrite){
+            return path
+        }
+        else {
+            file_name = path.split(".").slice(0,-1).join(".")
+            file_ext = path.split(".").pop()
+            let i = 0
+            while (fs.existsSync("./crawling_results/" + `${file_name}_${i}.${file_ext}`))
+                i = i + 1
+            return `${file_name}_${i}.${file_ext}`
+        }
+    },
 
-exports.increment_path = (path, overwrite = false) => {
-    if (overwrite){
-        return path
+    get_user_email: async (registerId) => {
+        let register = await Register.findOne( {where: {registerId: registerId}} )
+        let user = await User.findOne( {where: {id: register["userId"]}})
+        return user["email"]
+    },
+
+    make_email_title: async (registerId) => {
+        let register = await Register.findOne( {where: {registerId: registerId}})
+        return `[메추리알 알림] ${register["siteName"]} 사이트를 확인하세요!`
+    },
+
+    make_email_title_overdue: async (registerId) => {
+        let register = await Register.findOne( {where: {registerId: registerId}})
+        return `[메추리알 알림] ${register["siteName"]} 사이트에 대해 설정하신 알림 마감 날짜가 지났습니다.`
+    },
+
+    make_email_body: async (registerId) => {
+        let register = await Register.findOne( {where: {registerId: registerId}})
+        return `
+        ${register["siteName"]} 사이트를 확인하세요!
+        내가 등록한 사이트 바로가기: ${register["pageUrl"]}
+
+        요청하신 키워드는: ${register["key1"]}, ${register["key2"]}, ${register["key3"]}
+        키워드 포함 조건: ${register["notifyLogic"]}
+
+        알림 사유: 설정한 마감일 전 등록 조건 일치!
+
+        알림이 성공적으로 이루어졌으므로 등록은 삭제됩니다.
+
+        메추리알을 사용해주셔서 감사합니다.
+        `
+    },
+
+    make_email_body_overdue: async (registerId) => {
+        let register = await Register.findOne( {where: {registerId: registerId}})
+        return `
+        ${register["siteName"]} 사이트에 원하시는 조건의 업데이트가 이루어지지 않았습니다.
+        내가 등록한 사이트 바로가기: ${register["pageUrl"]}
+
+        요청하신 키워드는: ${register["key1"]}, ${register["key2"]}, ${register["key3"]}
+        키워드 포함 조건: ${register["notifyLogic"]}
+
+        알림 사유: 알림 마감 날짜 지남
+
+        설정하신 등록이 이제 무효하므로 등록은 삭제됩니다.
+
+        메추리알을 사용해주셔서 감사합니다.
+        `
     }
-    else {
-        file_name = path.split(".").slice(0,-1).join(".")
-        file_ext = path.split(".").pop()
-        let i = 0
-        while (fs.existsSync("./crawling_results/" + `${file_name}_${i}.${file_ext}`))
-            i = i + 1
-        return `${file_name}_${i}.${file_ext}`
-    }
-}
 
-exports.get_user_email = async (registerId) => {
-    let register = await Register.findOne( {where: {registerId: registerId}} )
-    let user = await User.findOne( {where: {id: register["userId"]}})
-    return user["email"]
-}
-
-exports.make_email_title = async (registerId) => {
-    let register = await Register.findOne( {where: {registerId: registerId}})
-    return `[메추리알 알림] ${register["siteName"]} 사이트를 확인하세요!`
-}
-
-exports.make_email_body = async (registerId) => {
-    let register = await Register.findOne( {where: {registerId: registerId}})
-    return `
-    ${register["siteName"]} 사이트를 확인하세요!
-    내가 등록한 사이트 바로가기: ${register["pageUrl"]}
-
-    요청하신 키워드는: ${register["key1"]}, ${register["key2"]}, ${register["key3"]}
-    키워드 포함 조건: ${register["notifyLogic"]}
-
-    알림 사유: 설정한 마감일 전 등록 조건 일치!
-
-    알림이 성공적으로 이루어졌으므로 등록은 삭제됩니다.
-
-    메추리알을 사용해주셔서 감사합니다.
-    `
 }
